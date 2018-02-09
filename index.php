8c454b78195e717e5e826b2f873d25e3ec2bf471
lyret <viktor@lyresten.se>
2018-02-09 11:24:08 +0100

    finished form

--- index.php
+++ index.php
@@ -1,2 +1,13 @@
-<?php
-    echo "Hello World";
+<?php include 'includes/header.php'; ?>
+
+<h2>Formuläret:</h2>
+<form>
+    <?php
+        foreach($listOfEachAttraction as $attraction) {
+            $attraction -> echoInputElement();
+            echo "<br/>";
+        }
+    ?>
+</form>
+
+<?php include 'includes/footer.php'; ?>
