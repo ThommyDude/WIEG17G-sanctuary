64e7d4c1b959a0234d6f5287380762c1ff973b52
lyret <viktor@lyresten.se>
2018-02-09 11:53:49 +0100

    can send form

--- index.php
+++ index.php
@@ -1,13 +1,14 @@
 <?php include 'includes/header.php'; ?>
 
 <h2>Formuläret:</h2>
-<form>
+<form action="results.php" method="POST">
     <?php
         foreach($listOfEachAttraction as $attraction) {
             $attraction -> echoInputElement();
             echo "<br/>";
         }
     ?>
+    <button type="submit">Skapa naturreservatet</button>
 </form>
 
 <?php include 'includes/footer.php'; ?>
